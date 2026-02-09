f21049b660681024a73c707f21c3ef88c96b8bc8
iamyosh <yoshedirisinghe28@gmail.com>
2026-02-09 13:01:35 +0530

    button with alert accepted

--- src/main/java/BodyPage.java
+++ src/main/java/BodyPage.java
@@ -16,6 +16,7 @@ public class BodyPage {
     private By textLink = By.id("alert2");
     private By popWindow = By.linkText("Open a popup window");
     private By uploadFile = By.id("uploadfile");
+    private By timerEnaleButton = By.id("timerButton");
 
     public BodyPage(WebDriver driver){
         this.driver = driver;
@@ -74,6 +75,9 @@ public class BodyPage {
         WebElement uploadElement = wait.until(ExpectedConditions.elementToBeClickable(uploadFile));
         uploadElement.sendKeys("C:\\Users\\MY FILE\\Downloads\\relational-database.png");  //send file path directly
 
+        //timer enable button + alert
+        wait.until(ExpectedConditions.elementToBeClickable(timerEnaleButton)).click();
+        driver.switchTo().alert().accept();
     }
 
 }
